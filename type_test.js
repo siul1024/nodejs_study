b79dc503267ecf60940900f6349ce3f798164ecd
siul1024 <hnong09@gmail.com>
2020-07-02 17:59:29 +0900

    commit

---
+++ type_test.js
@@ -0,0 +1,18 @@
+var arr = [1, 2, 3, 4, 5];
+
+console.log(arr);
+
+console.log(arr.length);
+
+arr.push(6);
+console.log(arr);
+console.log(arr.length);
+
+
+arr.pop(); //뒤에서부터 pop
+console.log(arr);
+console.log(arr.length);
+
+
+console.log(arr);
+console.log(arr.length);
