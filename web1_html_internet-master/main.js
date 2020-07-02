befebbab58f08e8d564bda2934d1857d840adc99
siul1024 <hnong09@gmail.com>
2020-07-02 17:10:51 +0900

    first commit

---
+++ web1_html_internet-master/main.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var server = http.createServer((request, response) => {
+    var _url = request.url;
+    
+    var queryData = url.parse(_url, true).query;
+    // console.log(queryData.id);
+    var title = queryData.id;
+    
+    if(_url == '/'){
+        title = 'Welcome!';
+    }
+    if(_url == '/favicon.ico'){
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+
+    fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, data) => {
+        var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title> WEB - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+        
+                ${data}
+        
+                </body>
+            </html>
+        `;
+        response.end(template);
+    });
+    //response.end(fs.readFileSync(__dirname + _url));
+    // response.end(queryData.id);
+});
+
+server.listen(3000);
