521857e9549978bdec7279e8fac8fb9bd45a05f4
siul1024 <hnong09@gmail.com>
2020-07-03 14:17:33 +0900

    commit

--- web/main.js
+++ web/main.js
@@ -2,57 +2,61 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function createIndexHTML(filelist){
+    var list = ``;
+    
+    for(key in filelist){
+        list += `<li><a href="/?id=${filelist[key]}">${filelist[key]}</a></li>`;
+    }
+
+    return list;
+}
+
+function basicTempletHTML(title, filelist, data){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+        <title> WEB - ${title}</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+
+        <ul>${filelist}</ul>
+
+        <h2>${title}</h2>
+
+        <p>${data}</p>
+
+        </body>
+    </html>    
+    `;
+}
+
 var server = http.createServer((request, response) => {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    // console.log(queryData.id);
     var title = queryData.id;
     
-    // if(_url === '/favicon.ico'){
-    //     return response.writeHead(404);
-    // }
-
-    if(pathname === '/'){
-        var pageindex = ``;
-
+    if(pathname === '/')
+    {
         fs.readdir('./data', (err, list) => {
-            console.log(list);
-            for(key in list){
-                pageindex += `<li><a href="/?id=${list[key]}">${list[key]}</a></li>`;
-            }
-
+            var indexTemplate = createIndexHTML(list);
+            
             fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
                 if(data === undefined){
                     title = 'Welcome!';
                     data = 'Hello Node js';
                 }
-                console.log(pageindex);
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                        <title> WEB - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-    
-                        <ul>${pageindex}</ul>
-    
-                        <h2>${title}</h2>
+                var baseTemplate = basicTempletHTML(title, indexTemplate, data);
                 
-                        <p>${data}</p>
-                
-                        </body>
-                    </html>
-                `;
-                // console.log(template);
                 response.writeHead(200);
-                response.end(template);
+                response.end(baseTemplate);
             });
         });
-        //response.end(fs.readFileSync(__dirname + _url));
+        // response.end(fs.readFileSync(__dirname + _url));
         // response.end(queryData.id);
     }
     else {
