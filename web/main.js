812cd505fefaa36d024b14147131e5a7b808d5dd
siul1024 <hnong09@gmail.com>
2020-07-06 13:46:41 +0900

    commit

--- web/main.js
+++ web/main.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
+let CREATE = 0;
+let UPDATE = 1;
+
 function createIndexHTML(filelist){
     var list = ``;
     
@@ -12,7 +15,7 @@ function createIndexHTML(filelist){
     return list;
 }
 
-function basicTempletHTML(title, filelist, body){
+function basicTempletHTML(title, filelist, body, handle){
     return `
         <!doctype html>
         <html>
@@ -24,14 +27,48 @@ function basicTempletHTML(title, filelist, body){
             <h1><a href="/">WEB</a></h1>
 
             <ul>${filelist}</ul>
-            <p><a href="/create">create</a></p>
+            <p>${handle}</p>
             <h2>${title}</h2>
+            
             <p>${body}</p>
 
             </body>
         </html>    
     `;
 }
+
+function basicTempletFormHTML(title, data, act){
+    if(act === 1){
+        return `
+            <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}"/>
+                <p>
+                <input type="text" name="title" placeholder="title" value="${title}" style="width:700px;" />
+                </p>
+                <p>
+                    <textarea name="content" placeholder="content" style="width:700px; height:200px; resize:none;">${data}</textarea>
+                </p>
+                <input type="submit"/>
+            </form>
+        `;
+    } else if(act === 0){
+        return `
+            <form action="/create_process" method="post">
+                <p>
+                <input type="text" name="title" placeholder="title" style="width:700px;" />
+                </p>
+                <p>
+                    <textarea name="content" placeholder="content" style="width:700px; height:200px; resize:none;"></textarea>
+                </p>
+                <input type="submit"/>
+            </form>
+        `;
+    }
+    return `
+        error
+    `;
+}
+
 
 var server = http.createServer((request, response) => {
     var _url = request.url;
@@ -47,41 +84,76 @@ var server = http.createServer((request, response) => {
                 if(data === undefined){
                     title = 'Welcome!';
                     data = 'Hello Node js';
-                }
-                var baseTemplate = basicTempletHTML(title, indexTemplate, data);
-                
+                    var baseTemplate = basicTempletHTML(title, indexTemplate, data, `<a href="/create">create</a>`);
+                } else {
+                    var baseTemplate = basicTempletHTML(title, indexTemplate, data, `
+                        <a href="/create">create</a> <a href="/update?id=${title}">update</a>
+                    `);
+                }                
                 response.writeHead(200);
                 response.end(baseTemplate);
             });
         });
+
     } else if(pathname === '/create'){
         title = "WEB - create";
         fs.readdir('./data', (err, list) => {
             var indexTemplate = createIndexHTML(list);
-            fs.readFile('./form.html', 'utf-8', (err, data) => {
-                var baseTemplate = basicTempletHTML(title, indexTemplate, data);
+            var form = basicTempletFormHTML(``, ``, CREATE);
+                var baseTemplate = basicTempletHTML(title, indexTemplate, form, ``);
+                response.writeHead(200);
+                response.end(baseTemplate);
+        });
+
+    } else if(pathname === '/create_process'){
+        var body = '';
+        request
+            .on('data', (data) => { body += data; }) 
+            .on('end', () => {
+                var post = qs.parse(body);
+                var title = post.title;
+                var content = post.content;
+                fs.writeFile(`./data/${title}`, content, 'utf-8', (err) => {
+                    response.writeHead(302, {Location: `/?id=${title}`});
+                    response.end();
+                });
+            });
+
+    } else if(pathname === '/update') {
+        fs.readdir('./data', (err, list) => {
+            var indexTemplate = createIndexHTML(list);
+            fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
+                var title = queryData.id;
+                var form = basicTempletFormHTML(title, data, UPDATE);
+                var baseTemplate = basicTempletHTML(title, indexTemplate, form, ``);
                 response.writeHead(200);
                 response.end(baseTemplate);
             });
         });
-    } else if(pathname === '/create_process'){
+
+    // file rename 처리할 방법..?
+    } else if(pathname === '/update_process'){
         var body = '';
-        request.on('data', (data) => {
-            body += data;
-        });  
-        request.on('end', () => {
-            var post = qs.parse(body);
-            var title = post.title;
-            var content = post.content;
-            fs.writeFile(`./data/${title}`, content, 'utf-8', (err) => {
-                response.writeHead(302, {Location: `/?id=${title}`});
-                response.end();
+        request
+            .on('data', (data) => { body += data; }) 
+            .on('end', () => {
+                var post = qs.parse(body);
+                // console.log(post);
+                var id = post.id;
+                var title = post.title;
+                var content = post.content;
+                fs.rename(`data/${id}`, `data/${title}`, (err) => { 
+                    fs.writeFile(`./data/${title}`, content, 'utf-8', (err) => {
+                        response.writeHead(302, {Location: `/?id=${title}`});
+                        response.end();
+                    });
+                });
             });
-        });
+
     } else {
         response.writeHead(404);
         response.end('Not Found');
     }
 });
 
-server.listen(8000);
+server.listen(8080);
